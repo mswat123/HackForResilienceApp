dc23e59bc660d7914b204fd7cc9e43c25f95533b
Arya Tschand <arya@voicesaver.com>
2018-09-07 23:45:02 -0400

    ColorChangeTest

---
+++ HackForResilienceApp/EmergencyContact.swift
@@ -0,0 +1,14 @@
+//
+//  EmergencyContact.swift
+//  HackForResilienceApp
+//
+//  Created by Arya Tschand on 9/7/18.
+//  Copyright © 2018 Manan Saaraswat. All rights reserved.
+//
+
+import Foundation
+
+class Person: Codable {
+    var name: String = ""
+    var phoneNumber: Int = 0
+}
--- HackForResilienceApp/EmergencyInput.swift
+++ HackForResilienceApp/EmergencyInput.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
+import MapKit
 
 class EmergencyInput: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,7 +24,7 @@ class EmergencyInput: UIViewController {
     
 
     /*
-    // MARK: - Navigation
+    // MARK: - Navigatio
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
