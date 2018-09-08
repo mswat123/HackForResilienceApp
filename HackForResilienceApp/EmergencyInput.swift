c731b96d30a1b480d98ddda695e23714f5df1635
Arya Tschand <arya@voicesaver.com>
2018-09-07 23:57:08 -0400

    Revert "recommit"
    This reverts commit d0908bf87c9d3f13c9f4e6c9d875899fe57ae22e.

--- HackForResilienceApp/EmergencyInput.swift
+++ HackForResilienceApp/EmergencyInput.swift
@@ -10,17 +10,26 @@ import UIKit
 
 class EmergencyInput: UIViewController {
     
-    @IBOutlet weak var UrgencySlider: UISlider!
-    @IBOutlet weak var UrgencyLabel: UILabel!
-    
-    @IBAction func SliderChanged(_ sender: UISlider) {
-        UrgencyLabel.text = String(UrgencySlider.value)
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigatio
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
