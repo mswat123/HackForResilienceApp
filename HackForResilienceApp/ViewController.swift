3c2576f8f6009700ded0e92c83caaee33aacaa6e
Manan Saaraswat <mananswat123@gmail.com>
2018-09-07 22:22:49 -0400

    Initial Commit

---
+++ HackForResilienceApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HackForResilienceApp
+//
+//  Created by Babita saaraswat on 9/7/18.
+//  Copyright © 2018 Manan Saaraswat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
